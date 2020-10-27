79f23637e63e2007d04e0cda4b51063a35eacc14
Rubens Leme <rubenss.leme@gmail.com>
2020-10-27 01:31:01 -0300

    Configuring  Eureka  Server

--- spring-microservices-curso-nelio-alves/hr-eureka-server/src/main/java/com/rubensleme/hreurekaserver/HrEurekaServerApplication.java
+++ spring-microservices-curso-nelio-alves/hr-eureka-server/src/main/java/com/rubensleme/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,8 @@ package com.rubensleme.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
